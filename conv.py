0ea1a36fcf1d4903296bc78791bdd63acb0e78bd
benj1r <47711426+benj1r@users.noreply.github.com>
2023-06-18 04:48:25 -0500

    convolution block code structure

---
+++ conv.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class ConvolutionBlock(nn.Module):
+    def __init__(self):
+        super(ConvolutionBlock, self).__init__()
+    
+    def forward():
+        pass
+
+
